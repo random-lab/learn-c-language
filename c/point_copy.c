34d557a7b735a0fe3e78767cde9a8543dd298669
random-lab <xxxxxx123@sina.cn>
2020-06-22 10:22:47 +0800

    how many bytes do the contents use?

---
+++ c/point_copy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+/* add some contents */
+
+	int x = 10;
+	int *p = &x;
+	printf("the address is %p\n",p);
+	printf("the value is %d\n",*p);
+
+	return 0;
+}
